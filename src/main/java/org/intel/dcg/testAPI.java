2e8a143a8ff9ad8d4586fd74d5f3211be6e7c94b
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-02-11 22:16:10 +0800

    init web visit

--- src/main/java/org/intel/dcg/testAPI.java
+++ src/main/java/org/intel/dcg/testAPI.java
@@ -1,7 +1,12 @@
 package org.intel.dcg;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class testAPI {
     public static void main(String[] args){
         System.out.println("Hello world");
+        SpringApplication.run(testAPI.class, args);
     }
 }
