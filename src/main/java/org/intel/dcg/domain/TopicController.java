26ee114ee94822c96fdd48e6065d0aa16921dec4
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-02-12 22:06:02 +0800

    add post method

--- src/main/java/org/intel/dcg/domain/TopicController.java
+++ src/main/java/org/intel/dcg/domain/TopicController.java
@@ -1,9 +1,8 @@
 package org.intel.dcg.domain;
 import org.intel.dcg.dao.Topic;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.intel.dcg.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +10,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/topics")
 public class TopicController {
+
+    //Auto DI the dependency
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("")
     public List<Topic> getTopic() {
-        List<Topic> myTopic = new ArrayList<Topic>();
-        myTopic.add(new Topic(1,"leslie","hhh"));
-        myTopic.add(new Topic(2,"mango","hhh2"));
-        return myTopic;
+        return topicService.getAllTopics();
     }
     @RequestMapping(value="/{topicID}",method = RequestMethod.GET)
     public Topic getTopicbyID(@PathVariable int topicID) {
-        return new Topic(topicID,"leslie","hhh");
+        return topicService.getTopic(topicID);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
---
+++ src/main/java/org/intel/dcg/service/TopicService.java
@@ -0,0 +1,34 @@
+package org.intel.dcg.service;
+
+import org.intel.dcg.dao.Topic;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> myTopics = new ArrayList<Topic>();
+
+    public TopicService(){
+        myTopics.add(new Topic(1,"leslie","hhh"));
+        myTopics.add(new Topic(2,"mango","hhh2"));
+    }
+
+    public List<Topic> getAllTopics(){
+        return myTopics;
+    }
+
+    public Topic getTopic(int id){
+        for(Topic topic:myTopics){
+            if(topic.getId() == id){
+                return topic;
+            }
+        }
+        return new Topic(id,"new leslie","new hhh");
+    }
+
+    public void addTopic(Topic topic){
+        myTopics.add(topic);
+    }
+}
