31bf406436d56825da25471a1a95916d83093b29
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-02-12 19:51:42 +0800

    add pathvaraiable

--- src/main/java/org/intel/dcg/domain/TopicController.java
+++ src/main/java/org/intel/dcg/domain/TopicController.java
@@ -1,5 +1,7 @@
 package org.intel.dcg.domain;
 import org.intel.dcg.dao.Topic;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,12 +9,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topics")
 public class TopicController {
-    @RequestMapping("/topics")
+    @RequestMapping("")
     public List<Topic> getTopic() {
         List<Topic> myTopic = new ArrayList<Topic>();
         myTopic.add(new Topic(1,"leslie","hhh"));
         myTopic.add(new Topic(2,"mango","hhh2"));
         return myTopic;
+    }
+    @RequestMapping(value="/{topicID}",method = RequestMethod.GET)
+    public Topic getTopicbyID(@PathVariable int topicID) {
+
+        return new Topic(topicID,"leslie","hhh");
     }
 }
